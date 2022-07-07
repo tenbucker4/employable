27a2c200143cd9dd4aaed9cbd8ba115ed6ade21a
Ben Tucker <bwtucker4@gmail.com>
2022-07-07 12:56:40 -0700

    Initial commit

---
+++ cv-project/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {};
+    }
+
+    render() {
+        return;
+    }
+}
+
+export default App;
